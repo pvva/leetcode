eb7e40fc9b5a92a69b7f539d448edd76b7968343
pavel.varyukhichev <dae.fromru@gmail.com>
2020-07-01 15:15:52 +0300

    add more solutions

---
+++ 0401-0500/441-arrangingCoins.go
@@ -0,0 +1,44 @@
+package main
+
+import "math"
+
+func main() {
+
+}
+
+/*
+https://leetcode.com/problems/arranging-coins/
+
+You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+Given n, find the total number of full staircase rows that can be formed.
+
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+
+Because the 4th row is incomplete, we return 3.
+*/
+
+// sum from 1 to n is n*(n+1)/2, so we just need to solve quadratic equasion for rounded down value
+func arrangeCoins(n int) int {
+	return int(math.Floor((math.Sqrt(8*float64(n)+1) - 1) / 2))
+}
